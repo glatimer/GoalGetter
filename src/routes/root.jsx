448b6836b5bb09f02a266cff4dc42ef53c64b479
Genevieve Latimer <glatimer@pdx.edu>
2024-05-16 16:39:16 -0700

    setting up routes once more.

---
+++ src/routes/root.jsx
@@ -0,0 +1,12 @@
+export function Root() {
+  <nav>
+    <ul>
+      <li>
+        <a href="/main"></a>;
+      </li>
+      <li>
+        <a href="/journal"></a>
+      </li>
+    </ul>
+  </nav>;
+}
