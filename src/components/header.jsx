e16964253b63c511030756e426909fc74990c00c
Genevieve Latimer <glatimer@pdx.edu>
2024-05-16 22:07:54 -0700

    Playing around with React icons for the nav bar. Needs styling.

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function Header() {
+  return (
+    <Box className="header">
+      <h1 color="#E4E932">RunMate</h1>
+    </Box>
+  );
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { FaPenToSquare } from "react-icons/fa6";
+import { IoHome } from "react-icons/io5";
 
 export function Root() {
   return (
     <nav>
       <ul>
         <li>
-          <a href="/main">Main</a>
+          <a href="/main">
+            <IoHome />
+          </a>
         </li>
+        <h1>RunMate</h1>
+        <h2>Every Step Counts</h2>
         <li>
-          <a href="/journal">Journal</a>
+          <a href="/journal">
+            <FaPenToSquare />
+          </a>
         </li>
       </ul>
     </nav>
