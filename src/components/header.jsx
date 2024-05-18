7cf269266e832312330917526afad915d08ad2f1
Genevieve Latimer <glatimer@pdx.edu>
2024-05-17 21:47:56 -0700

    Getting the Landing page styled.  Need to make nav items horizontal.  Need to make photo fluid fill the container.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function Header() {
   return (
     <div className="container">
-      <img src="./assets/horizon_run.png" alt="A runner passing" />
+      <img
+        style={{ objectFit: `cover`, backgroundColor: `#1a0841` }}
+        src="/horizon_run.png"
+        alt="A runner conquering a horizon."
+      />
     </div>
   );
 }
