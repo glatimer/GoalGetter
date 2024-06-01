4f62c1af234b94a5900720e27f2e15d8858e301e
Genevieve Latimer <glatimer@pdx.edu>
2024-06-01 10:44:28 -0700

    Added styling for home page and incorporated weather component.

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Navbar from "../Components/navbar";
 import QuoteDisplay from "../Components/quotes";
+import Weather from "../Components/weather";
 
 export function Home() {
   return (
     <>
-      <div id="home">
-        <Navbar />
-        <QuoteDisplay />
-      </div>
+      <body className="container-body">
+        <main className="home">
+          <div className="navbar">
+            <Navbar />
+          </div>
+          <div className="quotes">
+            <QuoteDisplay />
+          </div>
+          <div className="weather">
+            <Weather />
+          </div>
+        </main>
+      </body>
     </>
   );
 }
