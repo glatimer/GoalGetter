c9e609638af5d2a359900eca36bcbd72bfd0d538
Genevieve Latimer <glatimer@pdx.edu>
2024-06-03 14:02:43 -0700

    Converted the calculateAQI function to be universal for more concise and resuable code.

--- src/Components/airQuality.jsx
+++ src/Components/airQuality.jsx
@@ -1,25 +1,62 @@
 // airQulaity.jsx is a child component of weather.jsx that recieves data from the same fetch
 // I only want to grab the data for TODAY and display it in a gauge
-import { calculatePM2_5 } from "../utils/calculate-aqi";
+import { calculateAQI } from "../utils/calculate-aqi";
 import { aqiColors } from "../utils/air-polution-colors";
 
 export default function AirQuality({ aqiData }) {
-  const { co, no2, o3, so2, pm2_5, pm10 } = aqiData;
+  let { co, no2, o3, so2, pm2_5, pm10 } = aqiData;
   if (!aqiData) {
     return <div>Air quality data not found.</div>;
   }
 
   // for testing
-  console.log("co = ", co);
-  console.log("no2 = ", no2);
-  console.log("Po3 = ", o3);
-  console.log("so2 = ", so2);
   console.log("PM2.5 = ", pm2_5);
   console.log("PM10 = ", pm10);
+  console.log("co = ", co);
+  console.log("so2 = ", so2);
+  console.log("no2 = ", no2);
+  console.log("o3 = ", o3);
+
+  // Truncate each pollutant to prep for AQI calculation
+  pm2_5 = Number.parseFloat(pm2_5).toFixed(1);
+  pm10 = Math.round(pm10);
+  co = Number.parseFloat(co).toFixed(1);
+  so2 = Math.round(so2);
+  no2 = Math.round(no2);
+  o3 = Number.parseFloat(o3).toFixed(3);
+  // for testing
+  console.log("PM2.5 = ", pm2_5);
+  console.log("PM10 = ", pm10);
+  console.log("co = ", co);
+  console.log("so2 = ", so2);
+  console.log("no2 = ", no2);
+  console.log("o3 = ", o3);
 
   // find the highest count of all the pollutants
-  const pm2_5AQI = calculatePM2_5(pm2_5);
-  console.log("pm2.5 AQI = ", pm2_5AQI);
+  const pm2_5AQI = calculateAQI(pm2_5, "pm2_5");
+  const pm10AQI = calculateAQI(pm10, "pm10");
+  const coAQI = calculateAQI(co, "co");
+  const so2AQI = calculateAQI(so2, "so2");
+  const no2AQI = calculateAQI(no2, "no2");
+  const o3AQI = calculateAQI(o3, "o3");
+
+  const pollutantValues = [];
+
+  console.log(
+    "AQI",
+    "pm2.5:",
+    pm2_5AQI,
+    "pm10:",
+    pm10AQI,
+    "co:",
+    coAQI,
+    "so2:",
+    so2AQI,
+    "no2:",
+    no2AQI,
+    "o3:",
+    o3AQI
+  );
 
   // calculate air quality based on highest pollutant
   const aqi = 0;
--- src/utils/calculate-aqi.js
+++ src/utils/calculate-aqi.js
@@ -1,6 +1,7 @@
 // This file will contain all functions to calculate air quality index for each pollutant
 // using the breakpoints provided by https://www.airnow.gov/sites/default/files/2020-05/aqi-technical-assistance-document-sept2018.pdf pg. 10: table 5
 // Pollutants calculated => co, no2, o3, so2, pm2_5, pm10
+// Note to dev: At some point come back and make this more modular/less redundant
 
 import { breakpoints } from "./air-polution-breakpoints";
 
@@ -11,24 +12,42 @@ let BP_lo = 0;
 let I_hi = 0;
 let I_lo = 0;
 
-// Calculate the Air Quality Index (aqi) using Equation 1 on pg. 9
-function aqiFunction(C, BP_hi, BP_lo, I_hi, I_lo) {
+// // Map the pollutant name to number of decimals for truncation
+// const polNames = [
+//   { name: "pm2_5", decimal: 1 },
+//   { name: "pm10", decimal: 0 },
+//   { name: "co", decimal: 1 },
+//   { name: "so2", decimal: 0 },
+//   { name: "no2", decimal: 0 },
+//   { name: "o3", decimal: 3 },
+// ];
+
+// Air Quality Index (aqi) function using Equation 1 on pg. 9 of airnow.gov document
+export function aqiFunction(C, BP_hi, BP_lo, I_hi, I_lo) {
   aqi = ((I_hi - I_lo) / (BP_hi - BP_lo)) * (C - BP_lo) + I_lo;
   return Math.round(aqi); // Round to nearest integer
 }
 
-/* ---------------- Small Particulate Matter (< 2.5 micrometers) Index Calulation ---------------- */
-function calculatePM2_5(pm2_5Value) {
-  // Truncate to one decimal place
-  const pm2_5Concentration = Number.parseFloat(pm2_5Value).toFixed(1);
+// Calculate AQI for any pollutant => co, no2, o3, so2, pm2_5, pm10
+export function calculateAQI(C, pollutantName) {
+  //   // How many decimals to truncate for this pollutant?
+  //   let dec = 0;
+  //   polNames.forEach((element) => {
+  //     if (pollutantName === element.name) {
+  //       dec = element.decimal;
+  //     }
+  //   });
+  //   if (dec === undefined) {
+  //     return -1;
+  //   }
+
+  // Truncate pollutant value accordingly
+  //   const C = Number.parseFloat(pollutantValue).toFixed(dec);
 
   // Check breakpoints to find BP and I values
-  breakpoints.pm2_5.forEach((element) => {
+  breakpoints[pollutantName].forEach((element) => {
     // Find breakpoint range that C falls between, Set BP and I values
-    if (
-      pm2_5Concentration >= element.BP_low &&
-      pm2_5Concentration <= element.BP_high
-    ) {
+    if (C >= element.BP_low && C <= element.BP_high) {
       BP_lo = element.BP_low;
       BP_hi = element.BP_high;
       I_lo = element.I_low;
@@ -36,7 +55,7 @@ function calculatePM2_5(pm2_5Value) {
 
       console.log(
         "C:",
-        pm2_5Concentration,
+        C,
         "bpLO:",
         BP_lo,
         "bpHI:",
@@ -49,23 +68,25 @@ function calculatePM2_5(pm2_5Value) {
     }
   });
 
-  if ((aqi = aqiFunction(pm2_5Concentration, BP_hi, BP_lo, I_hi, I_lo))) {
+  // Check for null and return AQI
+  if ((aqi = aqiFunction(C, BP_hi, BP_lo, I_hi, I_lo))) {
     return aqi;
   } else {
     return 0;
   }
 }
 
-// carbon monoxide index calculation
-function calculateCo(coValue) {
-  // truncate to one decimal place
-  const coConcentration = Number.parseFloat(coValue).toFixed(1);
+// // carbon monoxide index calculation
+// export function calculateCo(coValue) {
+//   // truncate to one decimal place
+//   const coConcentration = Number.parseFloat(coValue).toFixed(1);
 
-  //
-}
+//   //
+// }
 
-export { calculatePM2_5, calculateCo };
+// export { calculatePM2_5, calculateCo };
 
 // Academic references:
 // truncate - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-// retrieve data from array of objects - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+// retrieve data from array of objects - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// round to integer - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
