e49b5092441f229ca252ce8e353cf75de2c90534
Genevieve Latimer <glatimer@pdx.edu>
2024-06-02 18:25:28 -0700

    update old commits

---
+++ src/Components/airQuality.jsx
@@ -0,0 +1,10 @@
+// airQulaity.jsx is a child component of weather.jsx that recieves data from the same fetch
+
+const airQuality = ({ weatherData }) => {
+  return (
+    <div className="air-quality">
+      <h2>Air Qaulity</h2>
+      {/* create visual with data and display */}
+    </div>
+  );
+};
