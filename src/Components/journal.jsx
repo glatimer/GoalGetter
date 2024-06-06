574b4a3470a61ef4c4600d09f8eaea49644342da
Genevieve Latimer <glatimer@pdx.edu>
2024-06-06 10:03:38 -0700

    swp errors.

---
+++ src/Components/journal.jsx
@@ -0,0 +1,11 @@
+// Journal component - a form submission (future iteration: that will store in a backend api)
+import React from "react";
+
+export default function Journal() {
+  // Get the date for the entry
+
+  // Get mile tracker data
+
+  // Get body entry text
+  return <div>Journal</div>;
+}
---
+++ src/routes/journal-page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Components/navbar";
+import Journal from "../Components/journal";
+
+export function Journal() {
+  return (
+    <>
+      {/* <main id="journal"> */}
+      <Navbar />
+      {/* <h1>This is the Journal Page</h1> */}
+      <Journal />
+      {/* </main> */}
+    </>
+  );
+}
